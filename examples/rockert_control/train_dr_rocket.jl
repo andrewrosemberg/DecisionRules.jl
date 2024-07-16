4341ad21512a09dba5b1f3c7d6b2fb4ab9bc04de
andrewrosemberg <andrewrosemberg@gmail.com>
2024-07-16 13:38:17 -0600

    update code

--- examples/rockert_control/train_dr_rocket.jl
+++ examples/rockert_control/train_dr_rocket.jl
@@ -103,13 +103,30 @@ objective_values = [simulate_multistage(
     det_equivalent, state_params_in, state_params_out, 
     final_state, sample(uncertainty_samples), 
     models;
-) for _ in 1:100]
+) for _ in 1:2]
 best_obj = mean(objective_values)
+
+example_dir = joinpath(pwd(), "examples", "rocket_control") #dirname(@__FILE__)
+
+model_dir = joinpath(example_dir, "models")
+save_file = "rocket_model"
+mkpath(model_dir)
+
+model_path = joinpath(model_dir, save_file * ".jld2")
+
+save_control = SaveBest(best_obj, model_path, 0.003)
 
 train_multistage(models, final_state, det_equivalent, state_params_in, state_params_out, uncertainty_samples; 
     num_batches=500,
     num_train_per_batch=1,
     optimizer=Flux.Adam(),
+    record_loss= (iter, model, loss, tag) -> begin
+        if tag == "metrics/training_loss"
+            save_control(iter, model, loss)
+        end
+        println("tag: $tag, Iter: $iter, Loss: $loss")
+        return false
+    end,
 )
 
 
